b30b6a469c184fb6ee27b3b337c6edfa19267fb9
mcarneir0 <76215464+mcarneir0@users.noreply.github.com>
2021-11-24 20:45:49 -0300

    Variáveis e comentários

---
+++ Variaveis_e_Comentarios.py
@@ -0,0 +1,9 @@
+x = -10      # uma variável do tipo inteiro (int)
+pi = 3.14    # uma variável do tipo real (float)
+escola = "Let's Code"   # uma variável literal (string)
+primeiraAula = True     # uma variável lógica (booleana)
+
+print(type(x))
+print(type(pi))
+print(type(escola))
+print(type(primeiraAula))
