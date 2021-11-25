5aa50ce6945baed933b2481351f53098d91303a6
mcarneir0 <76215464+mcarneir0@users.noreply.github.com>
2021-11-24 21:03:44 -0300

    Operadores aritméticos, relacionais e lógicos

---
+++ Operadores.py
@@ -0,0 +1,43 @@
+# Operadores Aritméticos
+
+a = 2 + 3   # Soma
+b = 2 - 3   # Subtração
+c = 2 * 3   # Multiplicação
+d = 2 / 3   # Divisão
+e = 2 // 3  # Divisão inteira
+f = 2 ** 3  # Potência
+g = 2 % 3   # Resto da divisão
+
+# Podemos fazer operações com variáveis numéricas e strings
+nome = input('Digite seu primeiro nome: ')
+nome = nome * 5
+print(nome)
+
+
+# Operadores Relacionais
+
+comparacao1 = 5 > 3    # Maior que
+comparacao2 = 5 < 3    # Menor que
+comparacao3 = 5 >= 3   # Maior ou igual
+comparacao4 = 5 <= 3   # Menor ou igual
+comparacao5 = 5 == 3   # Igual
+comparacao6 = 5 != 3   # Diferente
+
+print("comparacao1:", comparacao1)
+print("comparacao2:", comparacao2)
+print("comparacao3:", comparacao3)
+print("comparacao4:", comparacao4)
+print("comparacao5:", comparacao5)
+print("comparacao6:", comparacao6)
+
+
+# Operadores Lógicos
+
+comparacao01 = 5 > 3 and 6 > 3   # verdadeiro se condição 1 for verdadeira E condição 2 for verdadeira
+comparacao02 = 5 < 3 or 6 < 3   # verdadeiro se condição 1 for verdadeira OU condição 2 for verdadeira
+
+print("comparacao01:", comparacao01)
+print("comparacao02:", comparacao02)
+
+# O operador not serve para inverter o resultado de uma expressão lógica
+# not True = False      not False = True
